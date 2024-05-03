7745bcbac96a00305b0c7a1b8c4c7d65316e98a9
ftgyhuy7t66 <gs1yuri4523@hotmail.com>
2024-05-03 08:40:39 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-function jump(nums) {
-  let jumps = 0;
-  let currentEnd = 0;
-  let farthest = 0;
-  for (let i = 0; i < nums.length - 1; i++) {
-    farthest = Math.max(farthest, i + nums[i]);
-    if (i === currentEnd) {
-      jumps++;
-      currentEnd = farthest;
-    }
-  }
-  return jumps;
-}
+const firstName = "John";
+const lastName = "Doe";
+const fullName = firstName + " " + lastName;
+console.log(fullName); // John Doe
