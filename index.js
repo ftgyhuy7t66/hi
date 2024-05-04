18e3f0177edd1e56cc5be8d23b69b83712981d88
ftgyhuy7t66 <gs1yuri4523@hotmail.com>
2024-05-04 21:45:38 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const firstName = "John";
-const lastName = "Doe";
-const fullName = firstName + " " + lastName;
-console.log(fullName); // John Doe
+function combinationSum(candidates, target) {
+  const result = [];
+  backtrack([], 0, 0);
+  return result;
+  function backtrack(combination, start, sum) {
+    if (sum === target) {
+      result.push([...combination]);
+      return;
+    }
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      combination.push(candidates[i]);
+      backtrack(combination, i, sum + candidates[i]);
+      combination.pop();
+    }
+  }
+}
