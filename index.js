2cd39c8baacff5c6353bdfb7c61f5fb50a73f358
ftgyhuy7t66 <gs1yuri4523@hotmail.com>
2024-05-08 23:01:16 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
-  }
-  return arr;
-}
+const firstName = "John";
+const lastName = "Doe";
+const fullName = firstName + " " + lastName;
+console.log(fullName); // John Doe
