0f4745e6965de6cc0bff2060d3706af44df6ca1d
ftgyhuy7t66 <gs1yuri4523@hotmail.com>
2024-05-12 02:32:47 +0700

    demo task

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
-  }
-  return arr;
-}
+const factorial = (num) => {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
+};
