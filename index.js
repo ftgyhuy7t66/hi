a4a387e1ff85795c3eb31a8a353cd48ce5f24bc9
ftgyhuy7t66 <gs1yuri4523@hotmail.com>
2024-05-13 13:30:47 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function maxDepth(root) {
-  if (!root) return 0;
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+function findPeakElement(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[mid + 1]) left = mid + 1;
+    else right = mid;
+  }
+  return left;
 }
