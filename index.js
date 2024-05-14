5dee098b65d4a19d0bbf488681a18c237dc2c4e5
ftgyhuy7t66 <gs1yuri4523@hotmail.com>
2024-05-14 13:07:11 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function findPeakElement(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] < nums[mid + 1]) left = mid + 1;
-    else right = mid;
-  }
-  return left;
+function rotateArray(arr, n) {
+  return arr.slice(n).concat(arr.slice(0, n));
 }
